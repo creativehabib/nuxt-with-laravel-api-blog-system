1e54a592dbcf807c0360e7f217baedf2c4a5db97
unknown <shamolrahaman@gmail.com>
2024-05-12 15:41:39 +0600

    Complete

--- components/Header.vue
+++ components/Header.vue
@@ -117,8 +117,8 @@
             <!-- Dropdown menu -->
             <div class="hidden z-50 my-4 w-56 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown">
                 <div class="py-3 px-4">
-                    <span class="block text-sm font-semibold text-gray-900 dark:text-white">Neil sims</span>
-                    <span class="block text-sm text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                    <span class="block text-sm font-semibold text-gray-900 dark:text-white">{{ auth.getUser?.name }}</span>
+                    <span class="block text-sm text-gray-500 truncate dark:text-gray-400">{{ auth.getUser?.email }}</span>
                 </div>
                 <ul class="py-1 text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
                     <li>
@@ -153,7 +153,7 @@
                 </ul>
                 <ul class="py-1 text-gray-500 dark:text-gray-400" aria-labelledby="dropdown">
                     <li>
-                        <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                        <a href="#" @click.prevent="auth.logout()" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
                     </li>
                 </ul>
             </div>
